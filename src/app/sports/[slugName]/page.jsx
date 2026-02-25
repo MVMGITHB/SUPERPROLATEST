fa14ece47e2869ef6d620d05710ed445a131af79
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-02-25 16:12:46 +0530

    All  code updated

--- src/app/sports/[slugName]/page.jsx
+++ src/app/sports/[slugName]/page.jsx
@@ -4,41 +4,64 @@ import axios from "axios";
 
 
 export async function generateMetadata({ params }) {
-  const { slugName } = await params;
+  const { slugName } = params;
 
-  try {
-    const response = await axios.get(
-      `${base_url}/api/blog/getOneBlogByslug/${slugName}`
-    );
-    const post = response.data;
-    if (!post) {
-      return {
-        title: "Post not found",
-        description: "This blog post could not be found.",
-        // robots: {
-        //   index: false,
-        //   follow: false,
-        // },
-      };
-    }
+  const response = await axios.get(
+    `${base_url}/api/blog/getOneBlogByslug/${slugName}`
+  );
 
-     return {
-      title: `${post.mtitle}`,
-      description: `${post.mdesc}`,
-      metadataBase: new URL('https://supernpro.com/'),
-    alternates: {
-      canonical: './',
+  const post = response.data;
 
-    },
-    
-    };
-  } catch (error) {
+  if (!post) {
     return {
-      title: "Error loading post",
-      description: "An error occurred while fetching post data.",
+      title: "Post not found",
+      description: "This blog post could not be found.",
+      robots: { index: false, follow: false },
     };
   }
+
+  const url = `/sports/${slugName}`;
+  const imageUrl =
+    post?.mimage?.startsWith("http")
+      ? post.mimage
+      : `https://supernpro.com${post?.mimage || post?.image}`;
+
+  return {
+    metadataBase: new URL("https://supernpro.com"),
+
+    title: post.mtitle,
+    description: post.mdesc,
+
+    alternates: {
+      canonical: url,
+    },
+
+    openGraph: {
+      title: post.mtitle,
+      description: post.mdesc,
+      url: url,
+      type: "article",
+      images: [
+        {
+          url:`${base_url}${post.image}` || imageUrl,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: post.mtitle,
+      description: post.mdesc,
+      images: [imageUrl],
+    },
+  };
 }
+
+
+
+
 const page = async ({ params }) => {
   const { slugName } =await params;
 
